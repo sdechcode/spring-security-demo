16fadb51ce457d54d6f3748273ea7bc73d17c19a
Piseth <pisethsek9899@gmail.com>
2024-06-19 09:27:34 +0700

    add test object mapper

--- src/test/java/com/sdechcode/springsecuritydemo/SpringSecurityDemoApplicationTests.java
+++ src/test/java/com/sdechcode/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.sdechcode.springsecuritydemo;
 
+import com.sdechcode.springsecuritydemo.util.JsonUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,11 @@ class SpringSecurityDemoApplicationTests {
 
     @Test
     void contextLoads() {
+        Car c1 = new Car("Toyota", 20000);
+        String s = JsonUtil.object2Json(c1);
+        System.out.println(s);
     }
 
 }
+
+record Car(String model, Integer price){}
